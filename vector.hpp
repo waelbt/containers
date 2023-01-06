2404d43ed4437ef828259ece7fb5b68e90915e45
Wael Boutzougarte <waboutzo@e2r11p10.1337.ma>
2023-01-06 18:03:09 +0100

    assignment operator & removing the _size attribute && adding some getters

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: waboutzo <waboutzo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 10:55:56 by waboutzo          #+#    #+#             */
-/*   Updated: 2023/01/06 10:37:59 by waboutzo         ###   ########.fr       */
+/*   Updated: 2023/01/06 18:01:37 by waboutzo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,12 +34,11 @@ namespace ft
 			typedef typename std::iterator_traits<iterator>::difference_type	difference_type; //tmp
 			typedef size_t 														size_type;
 
-		private:
+		public:
 			pointer			_begin;
 			pointer			_end;
 			pointer			_end_cap;
 			allocator_type	_alloc;
-			size_type		size;
 
 		public:
 			explicit vector (const allocator_type& alloc = allocator_type());
@@ -48,23 +47,109 @@ namespace ft
 			// template <class InputIterator>
          	// vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
 			vector (const vector& x);
+			
+			vector& operator= (const vector& x);
+
+			~vector();
+
+			// iterator begin();
+			// const_iterator begin() const;
+			// iterator end();
+			// const_iterator end() const;
+
+			size_type size() const;
+			size_type max_size() const;
+			size_type capacity() const;
+			void resize (size_type n, value_type val = value_type());
 	};
+
 	template < class T, class Alloc>
-	vector<T, Alloc>::vector (const Alloc& alloc) : _begin(NULL), _end(NULL), _end_cap(NULL), _alloc(alloc), size(0){}
+	vector<T, Alloc>::vector (const Alloc& alloc) : _begin(NULL), _end(NULL), _end_cap(NULL), _alloc(alloc){}
 
 	template < class T, class Alloc>
 	vector<T, Alloc>::vector (size_type n, const value_type& val, const allocator_type& alloc)
-	: _alloc(alloc), size(n) {
-		_begin = _alloc.allocate(size);
+	: _alloc(alloc){
+		_begin = _alloc.allocate(n);
 		_end = _begin + n;
 		_end_cap = _end;
 		for(pointer i = _begin; i < _end; i++)
 			_alloc.construct(i, val);
 	}
 	template < class T, class Alloc>
-	vector<T, Alloc>::vector (const vector& x) : _begin(NULL), _end(NULL), _end_cap(NULL){
-		*this = x; // need to define the assignment
+	vector<T, Alloc>::vector (const vector& x) : _begin(NULL), _end(NULL), _end_cap(NULL), _alloc(x._alloc){
+		*this = x;
+	}
+	template < class T, class Alloc>
+	vector<T, Alloc>&  vector<T, Alloc>::operator= (const vector& x){
+		pointer		tmp;
+	
+		if (this != &x)
+		{
+			// not all the allocator protected
+			for(pointer i = _begin; i < _end; i++)
+				_alloc.destroy(i);
+			if (x.size() > this->capacity())
+			{
+				_alloc.deallocate(_begin, this->capacity());
+				_begin = _alloc.allocate(x.capacity());
+				_end_cap = _begin + x.capacity();
+			}
+			_end = _begin + x.size();
+			tmp = x._begin;
+			for(pointer i = _begin; i < _end; i++)
+				_alloc.construct(i, *(tmp++));
+		}
+		return *this;
+	}
+
+	template < class T, class Alloc>
+	size_t vector<T, Alloc>::capacity() const{
+		return (_end_cap - _begin);
+	}
+	
+	template < class T, class Alloc>
+	size_t vector<T, Alloc>::size() const{
+		return (_end - _begin);;
+	}
+	
+	template < class T, class Alloc>
+	size_t vector<T, Alloc>::max_size() const{
+		return  (SIZE_MAX / sizeof(T));
+	}
+	
+	template < class T, class Alloc>
+	void vector<T, Alloc>::resize (size_type n, value_type val){
+		//i will implement inserte or erase first
+	}
+	
+	template < class T, class Alloc>
+	vector<T, Alloc>::~vector(){
+		for(pointer i = _begin; i < _end; i++)
+			_alloc.destroy(i);
+		_alloc.deallocate(_begin, this->size());
 	}
 }
 
+
+
+	// template < class T, class Alloc>
+	// typename vector<T, Alloc>::iterator vector<T, Alloc>::begin(){
+	// 	return iterator(_begin);
+	// }
+
+	// template < class T, class Alloc>
+	// typename vector<T, Alloc>::const_iterator vector<T, Alloc>::begin() const{
+	// 	return const_iterator(_begin);
+	// }
+	
+	// template < class T, class Alloc>
+	// typename vector<T, Alloc>::iterator vector<T, Alloc>::end(){
+	// 	return iterator(_end);
+	// }
+
+	// template < class T, class Alloc>
+	// typename vector<T, Alloc>::const_iterator vector<T, Alloc>::end() const{
+	// 	return const_iterator(_end);
+	// }
+	
 #endif
